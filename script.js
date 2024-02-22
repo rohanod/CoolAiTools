88706d21b130a76bae1399ce90751a8538485e2b
rjt11221 <rohanliamodwyer+1@gmail.com>
2024-02-23 00:10:56 +0800

    Fixed search

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const searchBars = document.querySelectorAll('.search-bar'); // Target all searc
 
 searchBars.forEach(searchBar => {
     searchBar.addEventListener('keyup', (event) => {
-        const searchTerm = event.target.value.toLowerCase();
+        if (event.key === 'Enter') { // Check if the Enter key was pressed
+            const searchTerm = event.target.value.toLowerCase();
 
-        // Redirect to search page with results
-        const queryParams = new URLSearchParams({ q: searchTerm });
-        window.location.href = 'search.html?' + queryParams.toString();
+            // Redirect to search page with results
+            const queryParams = new URLSearchParams({ q: searchTerm });
+            window.location.href = 'search.html?' + queryParams.toString();
+        }
     });
 });
